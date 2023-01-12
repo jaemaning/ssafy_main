1b0df018ce0bbece5a352cad4c68259c53fd5c99
jaemaning <lovesay00@naver.com>
2023-01-12 11:13:25 +0900

    1,2일차 사피교육

---
+++ new.py
@@ -0,0 +1,21 @@
+# n = 0
+# while n < 3:
+#     print(n)
+#     n += 1
+
+
+# for i in range(1,9):
+#     print(f"2*{i}={2*i}")
+#
+#
+#
+# i = 1
+# while i < 10:
+#     print(f"2 x {i} = {2 * i}")
+#     i += 1
+
+dusts = [59, 24, 102]
+
+a=sorted(dusts)
+
+print(a[::-1])
