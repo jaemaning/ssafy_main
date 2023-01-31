8133d5ad987a35b20ca86cca83dd24bde836bf9c
jaemaning <lovesay00@naver.com>
2023-01-31 15:23:55 +0900

    ssafy 파일정리

--- ssafy수업내용/exception_.py
+++ ssafy수업내용/exception_.py
@@ -2,5 +2,10 @@
 try:
     num = int(input("숫자를 입력해주세요"))
     print(num)
-except:
+    print(10/num)
+except Exception:  ## 상위 예외 (valueerror, zerodivision error 다 포함)
+    print("에러발생")    
+except ValueError:
     print("숫자만 입력하세요")
+except ZeroDivisionError:
+    print("0으로 나눌수는 없습니다.")
